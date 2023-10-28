dbefa3b78f8291dda63afc316c1d9ff450009aea
itsbhavana <bhavanasuryawanshi1999@gmail.com>
2023-10-28 11:26:17 +0530

    New program added to find sum and average of 3 numbers

---
+++ SumAndAverageOfThreeNumbers.java
@@ -0,0 +1,15 @@
+class SumAndAverageOfThreeNumbers{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 25;
+        int c =56;
+
+        int sum = a + b + c;
+
+        double avg = (double)sum / 3;
+
+        System.out.println("The sum of three numbers is : " + sum);
+        System.out.println("The average of three numbers is : " + avg);
+
+    }
+}
